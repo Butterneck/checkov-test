b40112c6cb32ed76da5e12ef6288a92212ebbf01
Filippo Pinton <filippo.pinton@thron.com>
2023-04-12 17:54:56 +0200

    Add bucket

--- infra/checkov_test/checkov_test_stack.py
+++ infra/checkov_test/checkov_test_stack.py
@@ -1,7 +1,6 @@
 from aws_cdk import (
-    # Duration,
     Stack,
-    # aws_sqs as sqs,
+    aws_s3,
 )
 from constructs import Construct
 
@@ -10,10 +9,28 @@ class CheckovTestStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
-
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "CheckovTestQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        pipeline_artifact_bucket = aws_s3.CfnBucket(
+            self,
+            "PipelineArtifactBucket",
+            access_control="Private",
+            bucket_encryption=aws_s3.CfnBucket.BucketEncryptionProperty(
+                server_side_encryption_configuration=[
+                    aws_s3.CfnBucket.ServerSideEncryptionRuleProperty(
+                        server_side_encryption_by_default=aws_s3.CfnBucket.ServerSideEncryptionByDefaultProperty(
+                            sse_algorithm="AES256"
+                        )
+                    )
+                ]
+            ),
+            public_access_block_configuration=aws_s3.BlockPublicAccess.BLOCK_ALL
+        )
+        pipeline_artifact_bucket.cfn_options.metadata = {
+          'checkov': {
+            'skip': [
+              {
+                'id': 'CKV_AWS_18',
+                'comment': 'No need to ensure the S3 bucket has access logging enabled'
+              }
+            ]
+          }
+        }
